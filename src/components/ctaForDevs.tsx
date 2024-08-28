81ccfb46422e361b2645e4a60dcb95dfa86282d0
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2024-08-29 01:13:00 +0200

    added a cta for developers

---
+++ src/components/ctaForDevs.tsx
@@ -0,0 +1,66 @@
+export default function CtaForDevs() {
+  return (
+    <>
+      <section className="w-full flex bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center space-y-5">
+            <div className="inline-flex items-end justify-center w-full text-center mx-auto">
+              <img
+                src="https://cdn.devdojo.com/tails/avatars/024.jpg"
+                className="absolute transform translate-x-24 ml-6 rounded-full w-12 h-12 md:w-16 md:h-16 border-4 border-white"
+              />
+              <img
+                src="https://cdn.devdojo.com/tails/avatars/012.jpg"
+                className="absolute transform -translate-x-24 -ml-6 rounded-full w-12 h-12 md:w-16 md:h-16 border-4 border-white"
+              />
+              <img
+                src="https://cdn.devdojo.com/tails/avatars/029.jpg"
+                className="absolute transform -translate-x-16 rounded-full w-16 h-16 md:w-20 md:h-20 border-4 border-white"
+              />
+              <img
+                src="https://cdn.devdojo.com/tails/avatars/005.jpg"
+                className="absolute transform translate-x-16 rounded-full w-16 h-16 md:w-20 md:h-20 border-4 border-white"
+              />
+              <img
+                src="https://cdn.devdojo.com/tails/avatars/032.jpg"
+                className="rounded-full w-20 h-20 md:w-24 md:h-24 border-4 border-white relative"
+              />
+            </div>
+            <p className="mt-1 text-4xl font-extrabold text-gray-900  sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Join
+              <span className="px-2 py-1 relative inline-block">
+                <svg
+                  className="stroke-current bottom-0 absolute text-blue-300 -translate-x-2"
+                  viewBox="0 0 410 18"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6 6.4c16.8 16.8 380.8-11.2 397.6 5.602"
+                    stroke-width="12"
+                    fill="none"
+                    fill-rule="evenodd"
+                    stroke-linecap="round"
+                  ></path>
+                </svg>
+                <span className="relative">2TO3DAO</span>
+              </span>
+            </p>
+            <p className="max-w-3xl mt-5 mx-auto text-xl text-gray-500 ">
+              Ready to make the leap from Web2 to Web3? Join our community of
+              forward-thinking engineers and start your journey with 2to3DAO
+              today. Whether you want to learn, build, or innovate, we have a
+              place for you.
+            </p>
+            <a
+              href="https://t.me/+bVl_-O03y0ZiOTNk"
+              target="_blank"
+              className="inline-block px-6 py-3 mt-8 bg-blue-600  text-white font-semibold rounded hover:bg-blue-700 "
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
